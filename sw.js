4e71afc0620727ae488d6d71693e78a7a1202022
Tim Caswell <tim@creationix.com>
2019-08-23 19:34:53 +0000

    Make app offline and installable

---
+++ sw.js
@@ -0,0 +1,17 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+
+workbox.precaching.precacheAndRoute([
+  '/',
+  '/app.js',
+  '/pixi.min.js',
+  '/sprites.json',
+  '/sprites.png',
+  '/style.css',
+]);
+
+workbox.routing.registerRoute(
+  /.*/,
+  new workbox.strategies.CacheFirst()
+);
+
